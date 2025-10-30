6ff5d0ff0b974eae2f1065cc1c2fd9b04eae7833
nordico94 <albornoz.juan.main@gmail.com>
2025-10-30 19:19:28 -0300

    Agrego clase Persona & Cliente

---
+++ ideaTpIntegradorSimplificada/src/clases/Persona.java
@@ -0,0 +1,84 @@
+package clases;
+
+import java.time.LocalDate;
+
+
+public abstract class Persona {
+
+    private static int contador = 1;
+    private int idPersona;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String celular;
+    private LocalDate fechaAlta;
+
+    public Persona(String dni, String nombre, String apellido, String celular) {
+        this.idPersona = contador++;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celular = celular;
+        this.fechaAlta = LocalDate.now();
+    }
+
+    public Persona() {
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public LocalDate getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(LocalDate fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "idPersona=" + idPersona +
+                ", dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", celular='" + celular + '\'' +
+                ", fechaAlta=" + fechaAlta +
+                '}';
+    }
+}
+
